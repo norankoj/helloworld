7333c435a46a7cd26c42570a7b64d5520a5cef5d
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-03-12 10:43:16 +0900

    1st

---
+++ src/com/javaex/helloworld/MyAge.java
@@ -0,0 +1,20 @@
+package com.javaex.helloworld;
+
+public class MyAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int myage;
+  
+  myage=25;
+  System.out.println(myage);
+  
+  myage=26;
+  
+  System.out.println(myage);
+  
+  char ch1=65;
+  System.out.println(ch1);
+	}
+
+}
